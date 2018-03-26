2ad62aeede5b25af34df503c0663686bcaddb854
D7703_08 <d7703_08@D7703-08ui-iMac.local>
2018-03-26 15:48:22 +0900

    game

--- How many fingers 02/ViewController.swift
+++ How many fingers 02/ViewController.swift
@@ -8,16 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate{
     @IBOutlet weak var input: UITextField!
     @IBOutlet weak var resultLabel: UILabel!
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        input.delegate = self
     }
-
+ 
     @IBAction func guess(_ sender: Any) {
         //랜덤  숫자 생성
         let number = arc4random() % 6
@@ -29,12 +29,24 @@ class ViewController: UIViewController {
         } else {
              resultLabel.text = "실패!!! Try again!!!! 숨겨진 숫자는 현재\(number)"
         
-        
-        
-        
     }
     
 
 }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+       // input.resignFirstResponder()
+        view.endEditing(true)
+        
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        input.resignFirstResponder()
+        view.backgroundColor = UIColor.yellow
+        return true
+    
+    
+    }
+    
+    
 
 }
